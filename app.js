fac7b44f2c4fe7273d148970ec76879f9f63c3fb
Benjamin Dant <Benjamin Dant>
2017-04-03 10:22:14 +0200

    Files creation

---
+++ app.js
@@ -0,0 +1,1 @@
+console.log('Main app.js file');
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
     echo "Hello World";
 ?>
+<body>
+    <input type="text" width="500" height="200" />
+    <canvas width="500" height="200"><canvas>
+    <script src="app.js"></script>
+</body>
