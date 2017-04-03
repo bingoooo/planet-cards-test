76d597a8de84f1a15ba0e070d1577d6f86d434ce
Benjamin Dant <benjamindant@gmail.com>
2017-04-03 22:34:13 +0200

    Added form and image treatment

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
+
+/**
+ * Application main javascript file
+ */
+
 console.log('Main app.js file');
 
-
 // Main variables declaration
-var canvas = document.getElementById("canvas");
-canvas.width = 500;
-canvas.height = 200;
-var ctx = canvas.getContext("2d");
 
 var input = document.getElementById("input-text");
 var fontSize = window.getComputedStyle(input, null).getPropertyValue('font-size').replace('px', '');
-var fontFamily = window.getComputedStyle(input, null).getPropertyValue('font-family')
+var fontFamily = window.getComputedStyle(input, null).getPropertyValue('font-family');
 var fontSettings = fontSize + 'px ' + fontFamily;
 var width = input.scrollWidth;
-console.log(window.getComputedStyle(input, null).getPropertyValue('font-strech'));
+var height = 200;
+
+var canvas = document.getElementById("canvas");
+canvas.width = width;
+canvas.height = height;
+var ctx = canvas.getContext("2d");
+
+//Debug
+var divTest = document.getElementById('output-test');
 
 
-//Event when keyup in the input field
+/*************************************************************************************************************/
+//Events
+
+//Link mirroring function when keyup in the input-text
 input.addEventListener("keyup", function(event){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.font = fontSettings;
@@ -29,6 +40,87 @@ input.addEventListener("keyup", function(event){
     renderText(renderList);
 });
 
+//WIP
+//Link send message function to the 'Envoyer' button
+var send = document.getElementById("send");
+var nom = document.getElementById('nom');
+var prenom = document.getElementById('prenom');
+var email = document.getElementById('email');
+var adresse = document.getElementById('adresse');
+var cp = document.getElementById('cp');
+var required = document.getElementById('required_modal')
+var requiredEmail = document.getElementById('required_email');
+var sendingModal = $('#sending_modal');
+
+send.addEventListener("click", function(event){
+    console.log("clicked", event);
+
+    //Check if required fields has all been full filled
+    /*
+    var unfilled = false;
+    var testEmail = email.value.match(/@/g);
+    if(nom.value === ''){
+        nom.classList.add('alert');
+        unfilled = true;
+    } else {
+        nom.classList.remove('alert');
+    }
+    if(prenom.value === ''){
+        prenom.classList.add('alert');
+        unfilled = true;
+    } else {
+        prenom.classList.remove('alert');
+    }
+    if(email.value === '' || testEmail === null){
+        email.classList.add('alert');
+        requiredEmail.classList.remove('hidden');
+        unfilled = true;
+    } else {
+        email.classList.remove('alert');
+        requiredEmail.classList.add('hidden');
+    }
+    if(adresse.value === ''){
+        adresse.classList.add('alert');
+        unfilled = true;
+    } else {
+        adresse.classList.remove('alert');
+    }
+    if(cp.value === ''){
+        cp.classList.add('alert');
+        unfilled = true;
+    } else {
+        cp.classList.remove('alert');
+    }
+    if(unfilled){
+        required.classList.remove('hidden');
+        return;
+    } else {
+        required.classList.add('hidden');
+    }
+    */
+
+    //Convert canvas to image
+    var image = new Image();
+    image = canvas.toDataURL("image/png");
+    console.log(image);
+    divTest.innerHTML = '<img src="' + image + '" />';
+
+    //POST datas in back end for treatment
+
+    //Simulating sending message
+    sendingModal.classList.remove('hidden');
+    setTimeout(function(){
+        sendingModal.classList.add('hidden');
+    }, 2000);
+});
+
+
+/*************************************************************************************************************/
+
+
+
+/*************************************************************************************************************/
+//Functions
 //Divides a text with the Enter separator into a table
 function divideTextByEnter(text){
     if(text.length < 1 || typeof(text)!=='string'){
@@ -50,11 +142,16 @@ function divideTextByEnter(text){
     return table;
 }
 
-
 //Analyses the table values and divide text if greater than determined width
 function divideTableFitWidth(table, width){
+    console.log(table);
+    var renderTable = [];
+    var stringToEvaluate = "";
     var string = "";
     var maxLength = 0;
+    if(table === undefined){
+        return renderTable;
+    }
     for (var i = 0; i < 100; i++){
         string += "a";
         var metrics = ctx.measureText(string);
@@ -63,10 +160,9 @@ function divideTableFitWidth(table, width){
             break;
         }
     }
-    var renderTable = [];
-    var stringToEvaluate = "";
     table.forEach(function(data){
         if(data.length > maxLength){
+            data = data.replace('\n', '');
             var parts = parseInt(data.length / maxLength, 0);
             for(var i = 0; i <= parts; i++){
                 renderTable.push(data.substring(i*maxLength, (i+1)*maxLength));
@@ -78,7 +174,7 @@ function divideTableFitWidth(table, width){
     return renderTable;
 }
 
-//Render the text in a canvas
+//Render a table text in a canvas
 function renderText(text){
     if(text.length < 1 || typeof(text)!=='object'){
         return;
@@ -92,7 +188,7 @@ function renderText(text){
     }
 }
 
-
+//WIP
 //Divides a text into a table depending of the input width OPTIONAL
 function divideTextFitWidth(text, width){
     if(text.length < 1 || typeof(text)!=='string'){
@@ -105,3 +201,6 @@ function divideTextFitWidth(text, width){
 
     }
 }
+
+
+/*************************************************************************************************************/
