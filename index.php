b8b8ed05f367c1cb7158b5c301f01ac8030e8865
Benjamin Dant <Benjamin Dant>
2017-04-03 10:34:17 +0200

    added style.css stylesheet

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
     echo "Hello World";
 ?>
+<head>
+    <title>Planet Cards Test</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 <body>
     <input type="text" width="500" height="200" />
-    <canvas width="500" height="200"><canvas>
+    <canvas width="500" height="200"></canvas>
     <script src="app.js"></script>
 </body>
