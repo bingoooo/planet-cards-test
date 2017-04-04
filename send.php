905813838a3ddb99f8cdc20994ae792d26ad8fa9
Benjamin Dant <benjamindant@gmail.com>
2017-04-04 14:42:20 +0200

    POST values testing 0005

--- send.php
+++ send.php
@@ -8,8 +8,6 @@
     $adresse = $_POST['adresse'];
     $cp = $_POST['cp'];
     $image = $_POST['image'];
-    echo "$nom $prenom $email $adresse $cp $image";
-    exit();
     //Send email using PHPMailer you may configure a gmail SMTP or other 
     $mail = new PHPMailer;
     $mail->isSMTP();
@@ -22,12 +20,14 @@
     $mail->Username = USERNAME;
     $mail->Password = PASSWORD;
     $mail->setFrom(USERNAME, 'Benjamin Dant');
-    $mail->addAddress('sukkhato@hotmail.com', 'Benjamin Dant');
+    $mail->addAddress($email, $prenom.' '.$nom);
     $mail->Subject = "PHP Mailer Test";
     if(isset($_POST['image'])){
         $mail->Body = '<img src="' . $_POST['image'] . '"/>';
+        var_dump($mail); exit();
     } else {
         $mail->Body = 'No image sent';
+        var_dump($mail); exit();
     }
     $mail->AltBody = "Plain Text Message Body";
     if(!$mail->send()){
