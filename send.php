4b877c9a194fe48f9c4dfc0131a80dbd364c8370
Benjamin Dant <benjamindant@gmail.com>
2017-04-04 10:10:50 +0200

    Back end tests 0006

--- send.php
+++ send.php
@@ -18,31 +18,29 @@
     };
     */
 
-    //Variables echo test
-    echo DATABASE_URL;
-    //echo USERNAME;
-    //echo PASSWORD;
-    //Post email using PHPMailer from Google Account
     date_default_timezone_set('Etc/UTC');
 
     require '../PHPMailerAutoload.php';
 
     $mail = new PHPMailer;
-
     $mail->isSMTP();
-
     $mail->SMTPDebug = 2;
-
     $mail->Debugoutput = 'html';
-
     $mail->Host = 'smtp.gmail.com';
-
     $mail->Port = 587;
-
     $mail->SMTPSecure = 'tls';
-
     $mail->SMTPAuth = true;
-
-    $mail->Username = USERNAME;
-
-    $mail->Password = PASSWORD;
+    $mail->Username = 'sukkhato@gmail.com';
+    $mail->Password = 'Synou4ne';
+    $mail->setFrom('sukkhato@gmail.com', 'Benjamin Dant');
+    //$mail->addReplyTo('reply@mail.com', 'Some One');
+    $mail->addAddress('sukkhato@hotmail.com', 'Benjamin Dant');
+    $mail->Subject = "PHP Mailer Test";
+    //$mail->msgHTML(file_get_contents('file.html'), dirname(__FILE__));
+    $mail->AltBody = "Plain Text Message Body";
+    //$mail->addAttachment('file');
+    if(!$mail->send()){
+        echo "Mailer Error: ".$mail->ErrorInfo;
+    } else {
+        echo "Message sent !";
+    }
