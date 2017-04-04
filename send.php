de46334329fcb58da51f56a6bc34feb4c908ccbe
Benjamin Dant <benjamindant@gmail.com>
2017-04-04 09:57:04 +0200

    Back end tests 0004

--- send.php
+++ send.php
@@ -1,7 +1,8 @@
 <?php
     //Message treatment
 
-    //Post an email
+    //Post an email using mail() function from PHP default class, but needs sender_mail configuration impossible on heroku
+    /*
     if(isset($_POST['image'])){
         $to = 'sukkhato@hotmail.com';
         $subject = "Votre image du ".date('Y-m-d HH:mm:ss', time());
@@ -13,13 +14,34 @@
         } else {
             echo 'error sending image: '.$_POST['image'];
         };
-        echo "sent";
         return;
     };
+    */
 
-    //Mail test
-    if(mail('sukkhato@hotmail.com', 'pctest', 'some test message', 'From: admin@mail.com')){
-        echo 'sent';
-    } else {
-        echo 'error';
-    };
+    //Variables echo test
+    echo USERNAME;
+    echo PASSWORD;
+    //Post email using PHPMailer from Google Account
+    date_default_timezone_set('Etc/UTC');
+
+    require '../PHPMailerAutoload.php';
+
+    $mail = new PHPMailer;
+
+    $mail->isSMTP();
+
+    $mail->SMTPDebug = 2;
+
+    $mail->Debugoutput = 'html';
+
+    $mail->Host = 'smtp.gmail.com';
+
+    $mail->Port = 587;
+
+    $mail->SMTPSecure = 'tls';
+
+    $mail->SMTPAuth = true;
+
+    $mail->Username = USERNAME;
+
+    $mail->Password = PASSWORD;
