b176af1a7488ce0a31e8136b1e707b3f6cd55c82
Benjamin Dant <benjamindant@gmail.com>
2017-04-04 17:21:40 +0200

    Using heroku config vars and modified email body template

--- send.php
+++ send.php
@@ -4,8 +4,11 @@
     //Get values from heroku config vars
     $username = getenv('USERNAME');
     $password = getenv('PASSWORD');
+    $adminmail = getenv('ADMIN_MAIL');
+    $host = getenv('HOST');
+    $port = getenv('PORT');
 
-    //Check $_POST values
+    //Get values from $_POST
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
     $email = $_POST['email'];
@@ -18,23 +21,25 @@
     $mail->isSMTP();
     $mail->SMTPDebug = 2;
     $mail->Debugoutput = 'html';
-    $mail->Host = 'smtp.gmail.com';
-    $mail->Port = 587;
+    $mail->Host = $host;
+    $mail->Port = $port;
     $mail->SMTPSecure = 'tls';
     $mail->SMTPAuth = true;
     $mail->Username = $username;
     $mail->Password = $password;
-    $mail->setFrom($username, 'Benjamin Dant');
+    $mail->setFrom($adminmail, 'Benjamin Dant');
     $mail->addAddress($email, $prenom.' '.$nom);
     $mail->Subject = "PHP Mailer Test";
     if(isset($_POST['image'])){
-        $mail->Body = '<img src="' . $_POST['image'] . '"/>';
-        var_dump($mail); exit();
+        $mail->Body = "$prenom $nom,<br> You have recently created an image with our tool, thanks for using it"
+        ."Personnal Information:<br>$nom $prenom<br>$adresse <br>$cp"
+        .'<img src="' . $_POST['image'] . '"/>';
+        echo $mail->Body;
     } else {
         $mail->Body = 'No image sent';
-        var_dump($mail); exit();
     }
     $mail->AltBody = "Plain Text Message Body";
+    echo "Break in line 42<br>\n"; exit();
     if(!$mail->send()){
         echo "Mailer Error: ".$mail->ErrorInfo;
     } else {
